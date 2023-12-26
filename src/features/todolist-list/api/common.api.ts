fac03c05d7c0dcef81c6d32c0047b3ce2dc521d5
atryomkorshykau <butovichjunior@gmail.com>
2023-12-26 22:31:20 +0300

    deploy gh pages

--- src/features/todolist-list/api/common.api.ts
+++ src/features/todolist-list/api/common.api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': 'd97e2ed5-672f-4df7-ab8f-20c419d5b616'
+        'API-KEY': '88c70fdf-4e66-4d91-860b-59030fc7971d'
     }
 }
 export const instance = axios.create({
