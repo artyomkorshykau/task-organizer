fc0f4a42c9784c58f24397a2a0aac4c595504866
atryomkorshykau <butovichjunior@gmail.com>
2024-01-06 18:04:23 +0300

    fix routes

--- src/features/todolist-list/api/common.api.ts
+++ src/features/todolist-list/api/common.api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '88c70fdf-4e66-4d91-860b-59030fc7971d'
+        'API-KEY': '88c70fdf-4e66-4d91-860b-59030fc7971d',
     }
 }
 export const instance = axios.create({
