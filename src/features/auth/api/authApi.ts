e27adc304d05e48cd272f529958d016477799c39
atryomkorshykau <butovichjunior@gmail.com>
2024-01-06 18:06:58 +0300

    fix routes

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,24 +1,15 @@
-import { BaseResponseType } from "features/todolist-list/api/types/api.types";
-import { LoginParams } from "features/auth/ui/login/types/login.types";
-import { instance } from "features/todolist-list/api/common.api";
+import {BaseResponseType} from "features/todolist-list/api/types/api.types";
+import {LoginParams} from "features/auth/ui/login/types/login.types";
+import {instance} from "features/todolist-list/api/common.api";
 
 export const authAPI = {
-  auth(data: LoginParams) {
-    return instance.post<BaseResponseType<{ userId?: number }>>("auth/login", {
-      email: "butovichjunior@gmail.com",
-      password: '8mjfyrVNN5G!MW2',
-      rememberMe: false
-    });
-  },
-  me() {
-    return instance.get<BaseResponseType<{
-      id: 29875,
-      email: "butovichjunior@gmail.com",
-      login: "artyomKorshykau"
+    auth(data: LoginParams) {
+        return instance.post<BaseResponseType<{ userId?: number }>>('auth/login', data)
+    },
+    me() {
+        return instance.get<BaseResponseType<{ id: number, email: string, login: string }>>('auth/me')
+    },
+    logout() {
+        return instance.delete<BaseResponseType>('auth/login')
     }
-    >>("auth/me");
-  },
-  logout() {
-    return instance.delete<BaseResponseType>("auth/login");
-  }
-};
+}
